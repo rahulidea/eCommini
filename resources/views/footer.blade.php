b738d357d58a0baeaf355706a944812c1e4b74bd
rahulidea <rahulideasetters@gmail.com>
2022-06-18 00:48:11 +0530

    First Commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+      </div>
+    </div>
+  </nav>
